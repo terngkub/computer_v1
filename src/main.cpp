8db807e84ca65fb0f8a0c4610a78c349ec155e54
terngkub <terngnattapol@gmail.com>
2019-07-27 12:16:03 +0200

    take argument instead of input stream

--- src/main.cpp
+++ src/main.cpp
@@ -2,48 +2,14 @@
 #include <string>
 #include "interpreter.hpp"
 
-void test_token()
+int main(int argc, char** argv)
 {
-    Token token_num = {TokenType::NUMBER, 10.0};
-    std::cout << token_num << '\n';
-
-    Token token_opt = {TokenType::PLUS, '+'};
-    std::cout << token_opt << '\n';
-
-    Token token_var = {TokenType::VARIABLE, "Var"};
-    std::cout << token_var << '\n';
-}
-
-void test_lexer()
-{
-    std::string input;
-    getline(std::cin, input);
-
-    Lexer lexer(input);
-    auto token = lexer.get_next_token();
-
-    while (token->type != TokenType::ERROR && token->type != TokenType::END)
+    if (argc != 2)
     {
-        std::cout << *token << '\n';
-        token = lexer.get_next_token();
+        std::cerr << "usage: ./computor [string]\n";
+        return 1;
     }
-}
-
-void test_parser()
-{
-    std::string input;
-    getline(std::cin, input);
-
-    Lexer lexer(input);
-    Parser parser(lexer);
-    auto node = parser.parse();
-    std::cout << node;
-}
-
-void test_interpreter()
-{
-    std::string input;
-    getline(std::cin, input);
+    std::string input(argv[1]);
 
     Lexer lexer(input);
     Parser parser(lexer);
@@ -52,15 +18,10 @@ void test_interpreter()
     if (err)
     {
         std::cerr << err->message << '\n';
-        return ;
+        return 1;
     }
         
     auto interpreter = Interpreter(ast);
     std::cout << "input: " << input << "\n";
     interpreter.interpret();
-}
-
-int main()
-{
-    test_interpreter();
 }
